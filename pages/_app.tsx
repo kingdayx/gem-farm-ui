f5cf6dee5c70631845e5e8bdca9ea6e23c33d7f1
kingdayx <54965488+kingdayx@users.noreply.github.com>
2024-01-24 05:20:39 -0500

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,27 @@
-import React from "react"
-import Head from "next/head"
-import { ThemeProvider } from "theme-ui"
-import Router, { AppProps } from "next/dist/shared/lib/router/router"
-import dynamic from "next/dynamic"
+import React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "theme-ui";
+import Router, { AppProps } from "next/dist/shared/lib/router/router";
+import dynamic from "next/dynamic";
 
 // import "nprogress/nprogress.css" //styles of nprogress
 // import "normalize.css/normalize.css"
-import "@solana/wallet-adapter-react-ui/styles.css"
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 // @ts-ignore
-import withGA from "next-ga"
+import withGA from "next-ga";
 
-import defaultTheme from "../styles/theme"
+import defaultTheme from "../styles/theme";
 
 const WalletProvider = dynamic(
   () => import("../components/WalletProvider/WalletProvider"),
   {
     ssr: false,
   }
-)
+);
 
 function App(props: AppProps) {
-  const { Component, pageProps } = props
+  const { Component, pageProps } = props;
 
   // const [colorMode, setColorMode] = useColorMode()
 
@@ -33,11 +33,9 @@ function App(props: AppProps) {
         <link href="/fonts/fonts.css" rel="stylesheet" />
       </Head>
 
-      <WalletProvider>
-        <Component {...pageProps} />
-      </WalletProvider>
+      <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default withGA(process.env.NEXT_PUBLIC_GA_ID, Router)(App)
+export default withGA(process.env.NEXT_PUBLIC_GA_ID, Router)(App);
